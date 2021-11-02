4672e756e1f7564980c79a17943f96826b663138
Taylor Ramirez <tramirez@pcdworks.com>
2021-11-02 14:38:16 -0500

    hack to get index.html

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <nuxt-content :document="doc" />
+</template>
+
+<script>
+export default {
+async asyncData({ $content, params }) {
+  const doc = await $content('home').fetch()
+  return { doc }
+},
+head() {
+  return {
+    title: this.doc.title,
+    script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: this.doc.description,
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        content: this.doc.title,
+      },
+      {
+        hid: "og:description",
+        name: "og:description",
+        content: this.doc.description,
+      },
+    ]
+    }
+  }
+}
+</script>
