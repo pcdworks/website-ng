26f1c3009811a753bcf80e0f74e013ec5efbe366
Taylor Ramirez <tramirez@pcdworks.com>
2021-11-04 10:44:16 -0500

    Added YouTube and Vimeo

---
+++ components/Vimeo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class='embed-container'>
+      <iframe :src="link"
+              frameborder='0'
+              webkitAllowFullScreen
+              mozallowfullscreen
+              allowFullScreen>
+      </iframe>
+    </div>
+    <div class="below-text">
+      {{text}}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.below-text {
+  text-align: center;
+  padding-top: 3px;
+}
+
+.embed-container {
+  position: relative;
+  padding-bottom: 56.25%;
+  overflow: hidden;
+}
+
+.embed-container iframe,
+.embed-container object,
+.embed-container embed {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ components/YouTube.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="videoWrapper">
+      <iframe :src="link"
+              frameborder="0"
+              allow="accelerometer;
+                    autoplay;
+                    encrypted-media;
+                    gyroscope;
+                    picture-in-picture"
+                    allowfullscreen>
+      </iframe>
+    </div>
+    <div class="below-text">
+      {{text}}
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.below-text {
+  text-align: center;
+  padding-top: 3px;
+}
+
+.videoWrapper {
+    position: relative;
+    padding-bottom: 56.25%; /* 16:9 */
+    padding-top: 25px;
+    height: 0;
+}
+.videoWrapper iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+
+</style>
