d8b02eb51e669b02a95dd9d30a6f3e8bfd3ade14
Taylor Ramirez <tramirez@pcdworks.com>
2021-11-05 15:02:28 -0500

    Added initial team component

--- components/Team.vue
+++ components/Team.vue
@@ -1,28 +1,42 @@
 <template>
-  <div></div>
+<v-row
+      class="fill-height"
+      align="center"
+      justify="start"
+    >
+      <template v-for="(member, id) in members">
+        <v-col
+          :key="id"
+          cols="12"
+          md="4"
+        >
+          <team-member
+            :name="member.name"
+            :title="member.title"
+            :education="member.title"
+            :information="member.information"
+            :skills="member.skills"
+            :linkedin="member.linkedin"
+            :twitter="member.twitter"
+            :email="member.email"
+          ></team-member>
+        </v-col>
+      </template>
+</v-row>
 </template>
 
 <script>
+import TeamMember from './TeamMember.vue'
 export default {
+  components: { TeamMember },
   props: {
-    name: {
-      type: String,
-      default: '',
-    },
-    title: {
-      type: String,
-      default: '',
-    },
-    education: {
-      type: String,
-      default: '',
-    },
-    avatar: {
-      type: String,
-      default: '/images/pcd-simple-512.png',
-    },
+    members: {
+      type: Array,
+      default: () => { return [] }
+    }
   },
   data: () => {
+    return {}
   }
 }
 </script>
--- components/TeamMember.vue
+++ components/TeamMember.vue
@@ -1,13 +1,108 @@
 <template>
-  <div></div>
+  <v-card
+    elevation="4"
+    outlined
+    shaped
+  >
+    <v-card-text>
+      <v-list-item class="grow">
+        <v-list-item-avatar size="80" tile>
+          <v-img
+            class="elevation-6"
+            :alt="name"
+            :src="avatar"
+          ></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title>{{name}}</v-list-item-title>
+          <v-list-item-subtitle>{{title}}</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-tabs>
+        <v-tab v-if="information != ''">Information</v-tab>
+        <v-tab v-if="skills.length != 0">Skills</v-tab>
+        <v-tab-item v-if="information != ''" class="tabby">
+          <br/>
+          {{information}}
+        </v-tab-item>
+        <v-tab-item v-if="skills.length != 0" class="tabby">
+          <br/>
+          {{skills}}
+        </v-tab-item>
+      </v-tabs>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer/>
+      <a v-if="email != ''" :href="'mailto:' + email" target="_blank" class="mr-1">
+        <b-btn icon>
+          <v-icon>mdi-email</v-icon>
+        </b-btn>
+      </a>
+      <a v-if="linkedin != ''" :href="linkedin" target="_blank" class="mr-1">
+        <b-btn icon>
+          <v-icon>mdi-linkedin</v-icon>
+        </b-btn>
+      </a>
+      <a v-if="twitter != ''" :href="twitter" target="_blank" class="mr-1">
+        <b-btn icon>
+          <v-icon>mdi-twitter</v-icon>
+        </b-btn>
+      </a>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 export default {
-
+  props: {
+    name: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    email: {
+      type: String,
+      default: '',
+    },
+    education: {
+      type: String,
+      default: '',
+    },
+    avatar: {
+      type: String,
+      default: '/images/pcd-simple-512.png',
+    },
+    linkedin: {
+      type: String,
+      default: '',
+    },
+    twitter: {
+      type: String,
+      default: '',
+    },
+    information: {
+      type: String,
+      default: ''
+    },
+    skills: {
+      type: Array,
+      default: () => { return [] }
+    }
+  },
+  data: () => {
+    return {}
+  }
 }
 </script>
 
 <style>
+
+.tabby {
+  height: 180px;
+  overflow-y: scroll;
+}
 
 </style>
