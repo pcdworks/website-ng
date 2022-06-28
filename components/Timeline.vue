e4a86c7c5b06cad3e8d5b7e58ca1f06f4fdf570a
Taylor <zeitue@gmail.com>
2022-06-28 09:57:17 -0500

    Added Timeline component

---
+++ components/Timeline.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-timeline class="timeline" :dense="false">
+    <v-timeline-item v-for="(item, n) in timeline" :key="n" large>
+      <template #icon>
+        <v-avatar>
+          <img :src="item.image" />
+        </v-avatar>
+      </template>
+      <template #opposite>
+        <span>{{item.opposite}}</span>
+      </template>
+      <v-card class="elevation-2">
+        <v-card-title class="text-h5"> Lorem ipsum </v-card-title>
+        <v-card-text>
+            {{item.primary}}
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+  </v-timeline>
+</template>
+
+
+<script>
+export default {
+  name: 'Timeline',
+  props: {
+    timeline: {
+      type: Array,
+      default: () => [],
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.timeline {
+  position: relative;
+}
+</style>
