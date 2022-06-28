f3bd2aa9638c1294b2afd17074702206cc3a9a98
Taylor Ramirez <tramirez@pcdworks.com>
2022-06-28 13:06:19 -0500

    Fixed spacing issue

--- components/Lander.vue
+++ components/Lander.vue
@@ -64,25 +64,23 @@
                 <v-spacer />
                 <v-card-actions>
                   <v-row align="center">
-                    <v-col></v-col>
-                    <v-col>
-                      <v-tooltip top>
-                        <template #activator="{ on, attrs }">
-                        <v-btn
-                          x-large
-                          color="primary"
-                          :to="card.link"
-                          :aria-label="card.title"
-                          :alt="card.title"
-                          v-bind="attrs"
-                          v-on="on">
-                            Learn More
-                          </v-btn>
-                        </template>
-                        <span>Learn More About {{ card.title }}</span>
-                      </v-tooltip>
-                    </v-col>
-                    <v-col></v-col>
+                    <v-spacer/>
+                    <v-tooltip top>
+                      <template #activator="{ on, attrs }">
+                      <v-btn
+                        x-large
+                        color="primary"
+                        :to="card.link"
+                        :aria-label="card.title"
+                        :alt="card.title"
+                        v-bind="attrs"
+                        v-on="on">
+                          Learn More
+                        </v-btn>
+                      </template>
+                      <span>Learn More About {{ card.title }}</span>
+                    </v-tooltip>
+                    <v-spacer/>
                   </v-row>
 
                 </v-card-actions>
